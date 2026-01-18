07c56c0c90840ce48e58c53b91ed8f81b7250db5
ebinesh <ebinesh2511@gmail.com>
2026-01-18 18:11:36 +0530

    chore: add components barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+// src/components/index.ts
+export { default as CategoryFilter } from "./CategoryFilter";
+export { default as TagFilter } from "./TagFilter";
+export { default as AcronymCard } from "./AcronymCard";
